681f042a004607c0ece219379e0ee862c68626a4
최성준 <db970406@timmanage.com>
2022-09-08 09:43:31 +0900

    feat: Modify prop util functions

--- src/util/function/prop.ts
+++ src/util/function/prop.ts
@@ -1,65 +1,27 @@
+import { colorPalette, TColors } from "../csstype/color";
 import { IMarginPalette } from "../csstype/margin";
 import { IPaddingPalette } from "../csstype/padding";
-import { IProps } from "../type/prop";
+import { TDirection } from "../type/prop";
 
-// 각 Atomic에 맞는 props들을 반환해줄 것이다.
-export const getCorrectProps = (props: IProps) => {
-  const {
-    bgColor,
-    padding,
-    margin,
-    borderRadius,
-    borderColor,
-    color,
-    size,
-    gap,
-    width,
-    height,
-    bold,
-    justifyContent,
-    alignItems,
-    flexDirection,
-    disabled,
-  }: IProps = props;
-  let buttonProps = {
-    bgColor,
-    padding,
-    margin,
-    borderRadius,
-    gap,
-    justifyContent,
-    alignItems,
-    width,
-    disabled,
-  };
-  let textProps = { size, color, bold };
-  let iconProps = { size, color };
-  let inputProps = {
-    bgColor,
-    padding,
-    margin,
-    borderRadius,
-    borderColor,
-    width,
-  };
-  let boxProps = {
-    bgColor,
-    width,
-    height,
-    borderRadius,
-    borderColor,
-    margin,
-    padding,
-    gap,
-    justifyContent,
-    alignItems,
-    flexDirection,
-  };
-
-  return { buttonProps, textProps, iconProps, inputProps, boxProps };
+export const getBorderColor = (color?: TColors, direction?: TDirection) => {
+  const borderCss = color && `${colorPalette[color]} 1px solid`;
+  switch (direction) {
+    case "all":
+      return { border: borderCss };
+    case "left":
+      return { borderLeft: borderCss };
+    case "right":
+      return { borderRight: borderCss };
+    case "top":
+      return { borderTop: borderCss };
+    case "bottom":
+      return { borderBottom: borderCss };
+    default:
+      return { border: borderCss };
+  }
 };
 
-// margin or padding이 들어왔을 때 x축이냐 y축을 가려주는 역할을 한다.
+// margin or padding이 들어왔을 때 x축이냐 y축, top,bottom,left,right을 가려주는 역할을 한다.
 export const getDirection = (
   prop: string,
   palette: IPaddingPalette | IMarginPalette
@@ -71,7 +33,14 @@ export const getDirection = (
       return `${0} ${realSize}`;
     case "y":
       return `${realSize} ${0}`;
-
+    case "t":
+      return `${realSize} 0 0 0`;
+    case "r":
+      return `0 ${realSize} 0 0`;
+    case "b":
+      return `0 0 ${realSize} 0`;
+    case "l":
+      return `0 0 0 ${realSize}`;
     default:
       return realSize;
   }
