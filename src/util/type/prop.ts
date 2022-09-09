0e971a5d7d8c04ede8a9cf1607bf8e395eb1a002
최성준 <db970406@timmanage.com>
2022-09-09 19:28:00 +0900

    feat: Modify xxs,xs,sm,md,lg,xl,xxl type

--- src/util/type/prop.ts
+++ src/util/type/prop.ts
@@ -5,33 +5,29 @@ import { TAlignItems, TFlexDirection, TJustifyContent } from "../csstype/flex";
 import { TGap } from "../csstype/gap";
 import { THeights } from "../csstype/height";
 import { TMargin } from "../csstype/margin";
+import { TOpacity } from "../csstype/opacity";
 import { TPadding } from "../csstype/padding";
 import { TSize } from "../csstype/size";
 import { TWidths } from "../csstype/width";
 
+type TXXL = "xxl" | "xxl-t" | "xxl-r" | "xxl-b" | "xxl-l" | "xxl-x" | "xxl-y";
+type TXL = "xl" | "xl-x" | "xl-y" | "xl-t" | "xl-r" | "xl-b" | "xl-l";
+type TLg = "lg" | "lg-x" | "lg-y" | "lg-t" | "lg-r" | "lg-b" | "lg-l";
+type TMd = "md" | "md-x" | "md-y" | "md-t" | "md-r" | "md-b" | "md-l";
+type TSm = "sm" | "sm-x" | "sm-y" | "sm-t" | "sm-r" | "sm-b" | "sm-l";
+type TXS = "xs" | "xs-x" | "xs-y" | "xs-t" | "xs-r" | "xs-b" | "xs-l";
+type TXXS = "xxs" | "xxs-x" | "xxs-y" | "xxs-t" | "xxs-r" | "xxs-b" | "xxs-l";
+
 export type TFormatSMLXY =
   | "default"
-  | "xxl"
-  | "xxl-x"
-  | "xxl-y"
-  | "xl"
-  | "xl-x"
-  | "xl-y"
-  | "lg"
-  | "lg-x"
-  | "lg-y"
-  | "md"
-  | "md-x"
-  | "md-y"
-  | "sm"
-  | "sm-x"
-  | "sm-y"
-  | "xs"
-  | "xs-x"
-  | "xs-y"
-  | "xxs"
-  | "xxs-x"
-  | "xxs-y";
+  | TXXL
+  | TXL
+  | TLg
+  | TMd
+  | TSm
+  | TXS
+  | TXXS
+  | "none";
 
 export type TFormatSML =
   | "default"
@@ -41,7 +37,8 @@ export type TFormatSML =
   | "md"
   | "sm"
   | "xs"
-  | "xxs";
+  | "xxs"
+  | "none";
 
 export interface IProps {
   bgColor?: TColors;
@@ -60,4 +57,7 @@ export interface IProps {
   alignItems?: TAlignItems;
   flexDirection?: TFlexDirection;
   disabled?: boolean;
+  opacity?: TOpacity;
 }
+
+export type TDirection = "left" | "right" | "top" | "bottom" | "all";
