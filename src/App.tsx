dddcf7c7de31f093092a1b23dbc38e05ee150db5
최성준 <db970406@timmanage.com>
2022-09-28 20:44:03 +0900

    파일이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./organism/Login";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/pro-solid-svg-icons";
 import { far } from "@fortawesome/pro-regular-svg-icons";
-import ManageAccount from "./organism/ManageAccount";
+import Login from "./component/templates/login/Login";
 
 library.add(fas, far);
 
@@ -15,7 +13,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/manageaccount" element={<ManageAccount />} />
       </Routes>
     </BrowserRouter>
   );
